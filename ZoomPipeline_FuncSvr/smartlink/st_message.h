baea7733b1b64cc77cb45b7fe60f2db3ce01e8ab
goldenhawking <goldenhawking@163.com>
2014-01-15 23:23:59 +0800

    Some little fix

--- ZoomPipeline_FuncSvr/smartlink/st_message.h
+++ ZoomPipeline_FuncSvr/smartlink/st_message.h
@@ -9,8 +9,8 @@ typedef struct tag_smartlink_message{
     unsigned __INT16_TYPE__ Mark;    //Always be "0xAA55"
     unsigned __INT16_TYPE__ version; //Structure Version
     unsigned __INT32_TYPE__ SerialNum;
-    unsigned __INT32_TYPE__ source_id ; //0xffffffffffffffff means from server
-    unsigned __INT32_TYPE__ destin_id ; //0xffffffffffffffff means to server
+    unsigned __INT32_TYPE__ source_id ; //0xffffffff means from server
+    unsigned __INT32_TYPE__ destin_id ; //0xffffffff means to server
     struct tag_payload{
         unsigned __INT32_TYPE__ data_length; //Data at least have 2 byte, for heart-beating counter.
         __UINT8_TYPE__ data[2];
@@ -23,8 +23,8 @@ typedef struct tag_smartlink_message{
     unsigned __int16 Mark;    //Always be 0xAA55
     unsigned __int16 version; //Structure Version
     unsigned __int32 SerialNum;
-    unsigned __int32 source_id ; //0xffffffffffffffff means from server
-    unsigned __int32 destin_id ; //0xffffffffffffffff means to server
+    unsigned __int32 source_id ; //0xffffffff means from server
+    unsigned __int32 destin_id ; //0xffffffff means to server
     struct tag_payload{
         unsigned __int32 data_length; //Data at least have 2 byte, for heart-beating counter.
         unsigned __int8 data[2];

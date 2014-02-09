4823cb62b044188453778ed4155ada6c8e92a6fd
goldenhawking <goldenhawking@163.com>
2014-02-09 18:52:42 +0800

    Realize an method to trans server side msgs

--- FunctionalClientTest/maindialog.cpp
+++ FunctionalClientTest/maindialog.cpp
@@ -3,6 +3,7 @@
 #include "../ZoomPipeline_FuncSvr/smartlink/st_message.h"
 #include "../ZoomPipeline_FuncSvr/smartlink/st_msg_applayer.h"
 #include <QSettings>
+#include <time.h>
 using namespace SmartLink;
 MainDialog::MainDialog(QWidget *parent) :
     QDialog(parent),
@@ -17,6 +18,10 @@ MainDialog::MainDialog(QWidget *parent) :
     connect(client, SIGNAL(disconnected()),this,SLOT(on_client_disconnected()));
     connect(client, SIGNAL(error(QAbstractSocket::SocketError)),this, SLOT(displayError(QAbstractSocket::SocketError)));
     connect(client, SIGNAL(bytesWritten(qint64)), this, SLOT(on_client_trasferred(qint64)));
+
+    m_currentReadOffset = 0;
+    m_currentMessageSize = 0;
+
 
     QSettings settings("goldenhawking club","FunctionalClientTest",this);
     ui->lineEdit_ip->setText(settings.value("ip","localhost").toString());
@@ -67,9 +72,17 @@ void MainDialog::new_data_recieved()
     QTcpSocket * pSock = qobject_cast<QTcpSocket*>(sender());
     if (pSock)
     {
-        QByteArray array =pSock->readAll();
-        //in this example, we just do nothing but to display the byte size.
-        displayMessage(QString("client %1 Recieved %2 bytes.").arg((quintptr)pSock).arg(array.size()));
+        QByteArray block =pSock->readAll();
+        if (block.isEmpty()==false && block.size()>0)
+        {
+
+            do
+            {
+                m_currentReadOffset = filter_message(block,m_currentReadOffset);
+            }
+            while (m_currentReadOffset < block.size());
+            m_currentReadOffset = 0;
+        }
     }
 }
 void MainDialog::displayMessage(const QString &str)
@@ -91,7 +104,7 @@ void MainDialog::timerEvent(QTimerEvent * evt)
             char * ptr = array.data();
             SMARTLINK_HEARTBEATING * pMsg = (SMARTLINK_HEARTBEATING *)ptr;
             pMsg->Mark = 0xBEBE;
-            pMsg->tmStamp = 0;
+            pMsg->tmStamp = time(0)&0x00ffff;
             //3/10 possibility to send a data block to server
             client->SendData(array);
 
@@ -104,4 +117,167 @@ void MainDialog::on_pushButton_connect_clicked()
         client->connectToHost(ui->lineEdit_ip->text(),ui->lineEdit_Port->text().toUShort());
     else
         client->disconnectFromHost();
+}
+void MainDialog::on_pushButton_regisit_clicked()
+{
+    quint16 nMsgLen = sizeof(SMARTLINK_MSG_APP::tag_app_layer_header)
+            +sizeof(stMsg_HostRegistReq);
+    QByteArray array(sizeof(SMARTLINK_MSG) + nMsgLen - 1,0);
+    char * ptr = array.data();
+    SMARTLINK_MSG * pMsg = (SMARTLINK_MSG *)ptr;
+    SMARTLINK_MSG_APP * pApp = (SMARTLINK_MSG_APP *)(((unsigned char *)
+                                                      (ptr))+sizeof(SMARTLINK_MSG)-1
+                                                     );
+    pMsg->Mark = 0x55AA;
+    pMsg->version = 1;
+    pMsg->SerialNum = 0;
+    pMsg->Priority = 1;
+    pMsg->Reserved1 = 0;
+    pMsg->source_id = (quint32)((quint64)(0xffffffff) & 0xffffffff );
+
+    pMsg->destin_id = (quint32)((quint64)(0x00000001) & 0xffffffff );;
+
+    pMsg->data_length = nMsgLen;
+    pMsg->Reserved2 = 0;
+
+
+    pApp->header.AskID = 0x01;
+    pApp->header.MsgType = 0x1000;
+    pApp->header.MsgFmtVersion = 0x01;
+    QString strSerial = ui->plainTextEdit_boxSerialNum->toPlainText();
+    memcpy ( pApp->MsgUnion.msg_HostRegistReq.HostSerialNum,
+             strSerial.toStdString().c_str(),64);
+
+    //3/10 possibility to send a data block to server
+    client->SendData(array);
+}
+//!deal one message, affect m_currentRedOffset,m_currentMessageSize,m_currentHeader
+//!return bytes Used.
+int MainDialog::filter_message(const QByteArray & block, int offset)
+{
+    const int blocklen = block.length();
+    while (blocklen>offset)
+    {
+        const char * dataptr = block.constData();
+
+        //Recieve First 2 byte
+        while (m_currentMessageSize<2 && blocklen>offset )
+        {
+            m_currentBlock.push_back(dataptr[offset++]);
+            m_currentMessageSize++;
+        }
+        if (m_currentMessageSize < 2) //First 2 byte not complete
+            continue;
+
+        if (m_currentMessageSize==2)
+        {
+            const char * headerptr = m_currentBlock.constData();
+            memcpy((void *)&m_currentHeader,headerptr,2);
+        }
+
+        const char * ptrCurrData = m_currentBlock.constData();
+        //Heart Beating
+        if (m_currentHeader.Mark == 0xBEBE)
+        {
+            while (m_currentMessageSize< sizeof(SMARTLINK_HEARTBEATING) && blocklen>offset )
+            {
+                m_currentBlock.push_back(dataptr[offset++]);
+                m_currentMessageSize++;
+            }
+            if (m_currentMessageSize < sizeof(SMARTLINK_HEARTBEATING)) //Header not completed.
+                continue;
+            const SMARTLINK_HEARTBEATING * ptr = (const SMARTLINK_HEARTBEATING *)m_currentBlock.constData();
+            displayMessage(tr("Recieved Heart-beating msg sended %1 sec(s) ago.").
+                           arg((time(0)&0x00ffff)-(ptr->tmStamp)));
+            //This Message is Over. Start a new one.
+            m_currentMessageSize = 0;
+            m_currentBlock = QByteArray();
+
+            continue;
+        }
+        else if (m_currentHeader.Mark == 0x55AA)
+            //Trans Message
+        {
+            while (m_currentMessageSize< sizeof(SMARTLINK_MSG)-1 && blocklen>offset)
+            {
+                m_currentBlock.push_back(dataptr[offset++]);
+                m_currentMessageSize++;
+            }
+            if (m_currentMessageSize < sizeof(SMARTLINK_MSG)-1) //Header not completed.
+                continue;
+            else if (m_currentMessageSize == sizeof(SMARTLINK_MSG)-1)//Header just  completed.
+            {
+                const char * headerptr = m_currentBlock.constData();
+                memcpy((void *)&m_currentHeader,headerptr,sizeof(SMARTLINK_MSG)-1);
+
+                //continue reading if there is data left behind
+                if (block.length()>offset)
+                {
+                    qint32 bitLeft = m_currentHeader.data_length + sizeof(SMARTLINK_MSG) - 1
+                            -m_currentMessageSize ;
+                    while (bitLeft>0 && blocklen>offset)
+                    {
+                        m_currentBlock.push_back(dataptr[offset++]);
+                        m_currentMessageSize++;
+                        bitLeft--;
+                    }
+                    //deal block, may be send data as soon as possible;
+                    deal_current_message_block();
+                    if (bitLeft>0)
+                        continue;
+                    //This Message is Over. Start a new one.
+                    m_currentMessageSize = 0;
+                    m_currentBlock = QByteArray();
+                    continue;
+                }
+            }
+            else
+            {
+                if (block.length()>offset)
+                {
+                    qint32 bitLeft = m_currentHeader.data_length + sizeof(SMARTLINK_MSG) - 1
+                            -m_currentMessageSize ;
+                    while (bitLeft>0 && blocklen>offset)
+                    {
+                        m_currentBlock.push_back(dataptr[offset++]);
+                        m_currentMessageSize++;
+                        bitLeft--;
+                    }
+                    //deal block, may be processed as soon as possible;
+                    deal_current_message_block();
+                    if (bitLeft>0)
+                        continue;
+                    //This Message is Over. Start a new one.
+                    m_currentMessageSize = 0;
+                    m_currentBlock = QByteArray();
+                    continue;
+                }
+            } // end if there is more bytes to append
+        } //end deal trans message
+        else
+        {
+            displayMessage(tr("Client Send a unknown start Header %1 %2. Close client immediately.")
+                             .arg((int)(ptrCurrData[0])).arg((int)(ptrCurrData[1])));
+            m_currentMessageSize = 0;
+            m_currentBlock = QByteArray();
+            offset = blocklen;
+
+            this->client->disconnectFromHost();
+        }
+    } // end while block len > offset
+
+    return offset;
+}
+//!deal current message
+int MainDialog::deal_current_message_block()
+{
+    //First, get uuid as soon as possible
+    //then , Start deal to-server messages
+    //Server - deal messages
+
+    displayMessage(tr("Broadcast Message is not currently supported."));
+    m_currentBlock = QByteArray();
+
+
+    return 0;
 }
--- FunctionalClientTest/maindialog.h
+++ FunctionalClientTest/maindialog.h
@@ -4,7 +4,11 @@
 #include <QDialog>
 #include <QStandardItemModel>
 #include <QStandardItem>
+#include <QByteArray>
 #include "qghtcpclient.h"
+#include "../ZoomPipeline_FuncSvr/smartlink/st_message.h"
+#include "../ZoomPipeline_FuncSvr/smartlink/st_msg_applayer.h"
+using namespace SmartLink;
 namespace Ui {
 class MainDialog;
 }
@@ -22,6 +26,28 @@ private:
     QGHTcpClient * client;
     QStandardItemModel model;
     int nTimer;
+
+    //!Message Dealers
+protected:
+    //!deal one message, affect m_currentRedOffset,m_currentMessageSize,m_currentHeader
+    //!return bytes Used.
+    int filter_message(const QByteArray &, int offset);
+    int deal_current_message_block();
+
+    //data items
+protected:
+    //The current Read Offset, from m_list_RawData's beginning
+    int m_currentReadOffset;
+    //Current Message Offset, according to m_currentHeader
+    int m_currentMessageSize;
+    //Current un-procssed message block.for large blocks,
+    //this array will be re-setted as soon as some part of data has been
+    //dealed, eg, send a 200MB block, the 200MB data will be splitted into pieces
+    QByteArray m_currentBlock;
+    //current Header
+    SMARTLINK_MSG m_currentHeader;
+    SMARTLINK_MSG_APP m_current_app_header;
+
 public slots:
     void new_data_recieved();
     void on_client_trasferred(qint64);
@@ -31,6 +57,7 @@ public slots:
     void displayMessage(const QString &str);
 
     void on_pushButton_connect_clicked();
+    void on_pushButton_regisit_clicked();
 };
 
 #endif // MAINDIALOG_H

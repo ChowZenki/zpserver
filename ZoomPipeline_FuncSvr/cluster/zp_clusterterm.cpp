745bd50084169b8a8e54bfe6ad91b0e1b445628d
goldenhawking <goldenhawking@163.com>
2014-03-12 19:19:06 +0800

    PREPARE TO JOIN

--- ZoomPipeline_FuncSvr/cluster/zp_clusterterm.cpp
+++ ZoomPipeline_FuncSvr/cluster/zp_clusterterm.cpp
@@ -1,26 +1,24 @@
 #include "zp_clusterterm.h"
 namespace ZP_Cluster{
-zp_ClusterTerm::zp_ClusterTerm(const QString & name,int nTransThreads ,int nWorkingThreads,QObject *parent ) :
-    QObject(parent)
-  ,m_strTermName(name)
-{
-    m_pClusterEng = new ZPTaskEngine::zp_pipeline(this);
-    m_pClusterEng->addThreads(nWorkingThreads);
-    m_pClusterNet = new ZPNetwork::zp_net_ThreadPool(8192,this);
-    m_pClusterNet->AddClientTransThreads(nTransThreads);
+	zp_ClusterTerm::zp_ClusterTerm(const QString & name,QObject *parent ) :
+		QObject(parent)
+	  ,m_strTermName(name)
+	{
+		m_pClusterEng = new ZPTaskEngine::zp_pipeline(this);
+		m_pClusterNet = new ZPNetwork::zp_net_ThreadPool(8192,this);
 
-}
-void zp_ClusterTerm::StartListen(const QHostAddress &addr, int nPort)
-{
-    m_pClusterNet->AddListeningAddress(m_strTermName,addr,nPort,false);
+	}
+	void zp_ClusterTerm::StartListen(const QHostAddress &addr, int nPort)
+	{
+		m_pClusterNet->AddListeningAddress(m_strTermName,addr,nPort,false);
 
-}
-bool zp_ClusterTerm::JoinCluster(const QHostAddress &addr, int nPort)
-{
-    return m_pClusterNet->connectTo(addr,nPort);
-}
-bool zp_ClusterTerm::canExit()
-{
-    return m_pClusterEng->canClose() && m_pClusterNet->CanExit();
-}
+	}
+	bool zp_ClusterTerm::JoinCluster(const QHostAddress &addr, int nPort)
+	{
+		return m_pClusterNet->connectTo(addr,nPort);
+	}
+	bool zp_ClusterTerm::canExit()
+	{
+		return m_pClusterEng->canClose() && m_pClusterNet->CanExit();
+	}
 }

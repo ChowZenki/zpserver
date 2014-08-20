0118c40ea111bfa6fd64733cbf1f8dc566848684
goldenhawking <goldenhawking@163.com>
2014-08-20 15:52:04 +0800

    Add auto-start feature from master branch

--- ZoomPipeline_FuncSvr/main.cpp
+++ ZoomPipeline_FuncSvr/main.cpp
@@ -20,16 +20,35 @@ int main(int argc, char *argv[])
 	app.installTranslator(&appTranslator);
 
 	ZPMainFrame w;
-	//Command Line Args
+	w.show();
+
+	//!the main program arg formats:
+	/*!  ZoomPipeline_FuncSvr [<--autostart> [config file name] ]
+	  *  When start with no commandline arguments, the server will enter a dialog-controlled UI model.
+	  *  If the commandline args has been specified, this server will enter an aut-config and start model.
+	  *  Cmdline formats:
+	  *  --autostart  	specify the cluster address to join.
+	  *  config file has a same format with UI-Saved ini file. this file name should be surrounded with ""
+	  *  if there are spaces in filename.
+	*/
+	//Command Line Args, support batch auto-config and auto start.
 	if (argc>1)
 	{
-
+		QString configfile;
+		for (int i=1;i < argc;++i)
+		{
+			QString strArg = argv[i];
+			if (-1!=strArg.indexOf("--autostart"))
+			{
+				if (++i < argc)
+					configfile = argv[i];
+				w.LoadSettingsAndForkServer(configfile);
+				break;
+			}
+		}
 	}
-	else
-	{
 
-	}
-	w.show();
+
 	int pp = app.exec();
 	return pp;
 }
